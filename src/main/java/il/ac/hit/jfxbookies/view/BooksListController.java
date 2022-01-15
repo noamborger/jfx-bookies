5f885b4c74c4df8ebe1470282468164dfe1b1cc8
lior alvas <lior9070@gmail.com>
2022-01-15 19:45:08 +0200

    adding and deleting file.

---
+++ src/main/java/il/ac/hit/jfxbookies/view/BooksListController.java
@@ -0,0 +1,20 @@
+package il.ac.hit.jfxbookies.view;
+
+import il.ac.hit.jfxbookies.library.book.Book;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+//import javafx.application.Application;
+
+
+
+public class MainController {
+    @FXML
+    private TableView<Book> dataTable;
+    @FXML
+    private Button button;
+
+
+
+
+}
---
+++ src/main/java/il/ac/hit/jfxbookies/view/LoginController.java
@@ -0,0 +1,65 @@
+package il.ac.hit.jfxbookies.view;
+
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.table.TableUtils;
+import il.ac.hit.jfxbookies.JdbcDriverSetup;
+import il.ac.hit.jfxbookies.library.book.Book;
+import il.ac.hit.jfxbookies.library.managing.BorrowBook;
+import il.ac.hit.jfxbookies.person.Client;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage
+import javafx.stage.Window
+import lombok.extern.java.Log;
+
+import java.sql.SQLException;
+import java.util.Calendar;
+
+public class HelloController {
+    @FXML
+    private Label responseText;
+    @FXML
+    Button Login;
+    @FXML
+    protected void onHelloButtonClick() {
+        //welcomeText.setText("Welcome to JavaFX Application!");
+        responseText.setText("Username/Password incorrect");
+        try {
+            Calendar id = JdbcDriverSetup.getLookup(BorrowBook.class).queryForEq("id", 1).get(0).getDate();
+
+            System.out.println(id.get(Calendar.DAY_OF_MONTH) + "/" + (id.get(Calendar.MONTH) + 1) + "/" + id.get(Calendar.YEAR));
+            /*
+            Book book = JdbcDriverSetup.getLookup(Book.class).queryForEq("title", "Lior").get(0);
+            String id = "3333333";
+            Client client = Client.builder()
+                    .id(id)
+                    .address("safsa ")
+                    .email("a@a.c")
+                    .name("lior")
+                    .phone("22222")
+                    .build();
+            JdbcDriverSetup.getCreate(Client.class).create(client);
+            BorrowBook borrowbook = BorrowBook
+                    .builder()
+                    .client(client)
+                    .active(true)
+                    .book(book)
+                    .build();
+            JdbcDriverSetup.getCreate(BorrowBook.class).create(borrowbook);
+            */
+        } catch (SQLException e) {
+            System.err.println("error");
+            e.printStackTrace();
+        }
+    }
+    public void handleLoginButton() throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
+
+        Stage window = (Stage) Login.getScene().getWindow();
+        window.setScene(new Scene(root, 500,400));
+    }
+}
